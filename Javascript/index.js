a3dcd65571359f6fa28f0658bb6966cdabc9d466
Muskan <muskan.xyz1151@gmail.com>
2023-08-08 15:15:43 -0400

    Objects in JS

---
+++ Javascript/index.js
@@ -0,0 +1,44 @@
+/*let a = 67;
+ a = 56;
+ console.log(a);
+
+const pi = 3.14
+
+//number 
+
+let age = 25; // Number Literal 
+let x = 6.7;
+console.log(typeof x); 
+
+//string
+
+let name = 'Adrianne';
+name = "a";
+console.log(name);
+
+//boolean 
+
+isAuthenticated= true; //Boolean Literal 
+
+let p = undefined;
+console.log(typeof p); 
+
+//null
+
+let color = null;
+console.log(typeof color); 
+
+isAuthenticated = "B";
+console.log(typeof isAuthenticated); 
+*/
+
+let student = {
+    id : 101,
+    name : 'Ana'
+}
+
+student.name = 'Adam';
+
+property = 'id';
+student[property]=109;
+console.log(student);
