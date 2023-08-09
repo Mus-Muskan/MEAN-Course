12a3dbae81139b1ad5b216c61c016d90f03a295c
Muskan <muskan.xyz1151@gmail.com>
2023-08-09 11:42:04 -0400

    Loops in JS

--- Javascript/index.js
+++ Javascript/index.js
@@ -30,8 +30,9 @@ console.log(typeof color);
 
 isAuthenticated = "B";
 console.log(typeof isAuthenticated); 
-*/
 
+
+// Objects in JS
 let student = {
     id : 101,
     name : 'Ana'
@@ -42,3 +43,106 @@ student.name = 'Adam';
 property = 'id';
 student[property]=109;
 console.log(student);
+
+
+//Arrays in Js
+
+let students=['Ana' , 'Adam' , 'Sam' , 345];
+
+
+students[4] = 'Millie';
+console.log(students);
+
+
+
+//Types of DMS
+
+//1. Control flow statements
+
+//Simple-if
+let age = 1;
+
+if(age>=18)
+console.log("Eligible");
+else
+console.log("Not Eligible");
+
+//if-else
+
+let x=10;
+
+if(x>=2)
+x=x+3;
+else
+x=x+4;
+
+console.log(x);
+
+//if...else...if Ladder
+let marks = 98;
+
+if(marks>90)
+grade='A';
+else if(marks>75)
+grade='B';
+else if(marks>50)
+grade='C';
+else
+grade='D';
+
+console.log(grade);
+
+//Ternary Operator
+let result;
+result = (6<7) ? true : false;
+console.log(result);
+
+//Loops in Js
+
+//1. for
+//2. while
+//3. do...while
+
+for(let i = 0; i<10 ; i++)
+{
+    console.log("Welcome to this course!");
+}
+
+let i=10;
+while(i>0)
+{
+    console.log(i);
+    i--;
+}
+
+let k=10;
+do{
+    console.log(k);
+    k--;
+}while(k<0);
+
+//Loops in Arrays & Objects
+
+let enrolledStudents = {
+    id : 101,
+    name : 'Ana'
+}
+
+//for..in loop
+
+for(let key in enrolledStudents){
+    console.log(key , enrolledStudents[key]);
+}
+
+let Marks = [23 , 56 , 78];
+for(let index in Marks){
+    console.log(index , Marks[index]);
+}
+
+//for..of loop
+
+for(let mark of Marks){
+    console.log(index);
+}
+
+*/
